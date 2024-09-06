c3e269c8345ce23bcd5dd9c54449de86ce4ac9be
Mzhafir45 <zafiralkamil@gmail.com>
2024-09-06 21:17:04 +0700

    initial commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,7 +46,7 @@
                     </svg>
                 </div>
                 <div class="japir">
-                    <h1>Muhammad Zhafir Al Kamil - 2217051094</h1>
+                    <h1>Imam Ahdy Sabilla - 2217051141</h2>
                 </div>
                 <div class="mt-16">
                     <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
